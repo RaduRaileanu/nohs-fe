399da965b3fea5ab1ea7c4550f017fa000c750ae
RaduRaileanu <67751805+RaduRaileanu@users.noreply.github.com>
2024-02-20 07:08:57 +0200

    Persist userStore in session storage

--- nohs-fe/src/stores/userStore.js
+++ nohs-fe/src/stores/userStore.js
@@ -4,5 +4,8 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('userStore', {
   state: () => ({
     token: ''
-  })
+  }),
+  persist: {
+    storage: sessionStorage
+  }
 })
